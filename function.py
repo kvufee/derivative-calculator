4e99992915971996eec457c35eadb64aef8a8fa9
wibbleydock <pzns1337@gmail.com>
2022-12-15 23:52:12 +0300

    [MODIFY] add imports and create class InputParser

--- function.py
+++ function.py
@@ -1,4 +1,15 @@
 import re
 import sympy as sp
 import numpy as np
+from sympy.abc import x
 
+from . import Parser
+
+
+class InputParser(Parser):
+
+    def __init__(self) -> None:
+        pass
+
+    def parsing(self, text: str) -> str:
+        match
